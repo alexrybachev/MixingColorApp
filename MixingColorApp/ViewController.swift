e7571a0eaf56120298315fa02cd4dd7a9d88b63b
Alex Rybachev <rybachev.as@gmail.com>
2021-03-27 09:27:11 +0400

    final

--- MixingColorApp/ViewController.swift
+++ MixingColorApp/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewColor: UIView!
+    
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewColor.layer.cornerRadius = 10
+        viewColor.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+        
+        redValue.textAlignment = .right
+        greenValue.textAlignment = .right
+        blueValue.textAlignment = .right
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
     }
 
-
+    @IBAction func mixingRedColor() {
+        redValue.text = String(round(redSlider.value * 100) / 100 )
+        let redColor = CGFloat(redSlider.value)
+        let greenColor = CGFloat(greenSlider.value)
+        let blueColor = CGFloat(blueSlider.value)
+                
+        viewColor.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    }
+    
+    @IBAction func mixingGreenColor() {
+        greenValue.text = String(round(greenSlider.value * 100) / 100 )
+        let redColor = CGFloat(redSlider.value)
+        let greenColor = CGFloat(greenSlider.value)
+        let blueColor = CGFloat(blueSlider.value)
+                
+        viewColor.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    }
+    
+    @IBAction func mixingBlueColor() {
+        blueValue.text = String(round(blueSlider.value * 100) / 100 )
+        let redColor = CGFloat(redSlider.value)
+        let greenColor = CGFloat(greenSlider.value)
+        let blueColor = CGFloat(blueSlider.value)
+                
+        viewColor.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    }
 }
 
